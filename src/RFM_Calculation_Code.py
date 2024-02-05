cf7bc585f8254dc61256ac9b47131a89acc64640
RuthvikaReddyTangirala <ruthvikareddytangirala20@gmail.com>
2024-02-05 11:54:06 -0500

    Adding Data_Insights code

--- src/RFM_Calculation_Code.py
+++ src/RFM_Calculation_Code.py
@@ -12,6 +12,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import datetime as dt
 from mpl_toolkits.mplot3d import Axes3D
 
 #reading the preprocessed dataset
